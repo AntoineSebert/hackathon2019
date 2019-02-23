814218e3e9c2815d0d63423757fba02bb76cbf5f
Antoine <antoine.sb@orange.fr>
2019-02-23 19:16:01 +0000

    :art: create empty functions to structure the program

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@
 import argparse
 
 def get_input_file():
+	"""Get the filepath from the command line."""
 	parser = argparse.ArgumentParser(description = 'Generate texts from text datatset.')
 	parser.add_argument(
 		"-f",
@@ -17,12 +18,30 @@ def get_input_file():
 	args = parser.parse_args()
 	return args.file
 
+def load_data(file):
+	"""Extract the data from the file and return it as a list of objects."""
+	return []
+
+def create_neural_network():
+	"""Create a neural network that takes a dataset of texts as input and generates texts based on the dataset."""
+	return {}
+
+def train_neural_network(neural_network):
+	"""Train the neural network using the provided dataset."""
+
+def generate_text(neural_network):
+	"""Generate new text."""
+	return {}
+
 def main():
 	"""Script entry point"""
 	print('hello world')
 
 	file = get_input_file()
-	#dataset = load_data(file)
+	dataset = load_data(file)
+	neural_network = create_neural_network()
+	train_neural_network(neural_network)
+	generate_text(neural_network)
 
 if __name__ == "__main__":
 	main()
