b3a5f8e5036e154b0a3e31fe6c1653d68067542b
Antoine <antoine.sb@orange.fr>
2019-02-23 19:02:35 +0000

    :sparkles: support command line argument for input file

--- src/main.py
+++ src/main.py
@@ -1,7 +1,28 @@
 """This module generates stories from sample using deep learning."""
 
-import tensorflow
-import keras
-import numpy
+import argparse
 
-print('hello world')
+def get_input_file():
+	parser = argparse.ArgumentParser(description = 'Generate texts from text datatset.')
+	parser.add_argument(
+		"-f",
+		"--file",
+		nargs = '?',
+		default = '../dataset/booksummaries/booksummaries.txt',
+		type = argparse.FileType('r'),
+		required = True,
+		dest = "file",
+		help = "use dataset in FILE"
+	)
+	args = parser.parse_args()
+	return args.file
+
+def main():
+	"""Script entry point"""
+	print('hello world')
+
+	file = get_input_file()
+	#dataset = load_data(file)
+
+if __name__ == "__main__":
+	main()
