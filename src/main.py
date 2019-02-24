ff6412c5b0e0f1feaa4439dcb787a8fcbcd02949
Antoine <antoine.sb@orange.fr>
2019-02-24 04:27:11 +0000

    :sparkles: print interesting parts of articles

--- src/main.py
+++ src/main.py
@@ -9,8 +9,7 @@ from html.entities import name2codepoint
 import numpy as np
 from keras.datasets import reuters
 from keras.models import Sequential
-from keras.layers import Dense
-from keras.layers import LSTM
+from keras.layers import Dense, LSTM
 from keras.layers.embeddings import Embedding
 from keras.preprocessing import sequence
 from keras.preprocessing import text
@@ -31,23 +30,21 @@ class article:
 	def __init__(self, html_content):
 		self.content = html_content
 
-class MyHTMLParser(HTMLParser):
+class LinksParser(HTMLParser):
 	def __init__(self):
 		HTMLParser.__init__(self)
 		self.recording = 0
 		self.data = []
 	def handle_starttag(self, tag, attrs):
-		for attr in attrs:
-			print("     attr:", attr)
-		if tag == 'p':
-			print("Start tag:", tag)
-			return
-	def handle_endtag(self, tag):
-		if tag == 'p':
-			print("End tag  :", tag)
-			return
+		for (key, value) in attrs:
+			if key == 'class' and 'graf--p' in value:
+				#print(tag, '=', key, ':', value, ' ')
+				self.recording = 1
+			else:
+				self.recording = 0
 	def handle_data(self, data):
-		print("Data     :", data)
+		if self.recording == 1:
+			print('    ' + data)
 
 def get_input_file():
 	"""Get the filepath from the command line."""
@@ -56,7 +53,7 @@ def get_input_file():
 		"-f",
 		"--file",
 		nargs = '?',
-		default = 'dataset/medium-articles/train.json',
+		default = 'dataset/medium-articles/test.json',
 		type = argparse.FileType('r'),
 		dest = "file",
 		help = "use dataset in FILE"
@@ -68,9 +65,13 @@ def get_input_file():
 def load_data(file):
 	"""Extract the data from the file and return it as a list of objects."""
 	articles = []
-	parser = MyHTMLParser()
+	parser = LinksParser()
 
+	limit = 0
 	for line in iter(lambda: file.readline(), ''):
+		if 1 < limit:
+			break
+		limit += 1
 		article_as_json = json.loads(line)
 		parser.feed(article_as_json["content"])
 		print(parser.data)
@@ -79,14 +80,16 @@ def load_data(file):
 
 	return articles
 
-def create_datasets(raw_data, top_words, max_review_length):
+def create_datasets(articles, top_words, max_review_length):
 	"""Create the train and test datasets, each entry being limited in length."""
 
-	# to replace
-	(x_train, y_train), (x_test, y_test) = reuters.load_data(num_words = top_words)
-	# iterate through list of articles and get their content
-	#x_train = text.text_to_word_sequence(x_train)
-	#x_test = text.text_to_word_sequence(x_test)
+	(x_train, y_train), (x_test, y_test) = reuters.load_data(num_words = top_words, test_split=0.2)
+
+	for article in range(0, int(len(articles) * 0.8)):
+		x_train.append(text.text_to_word_sequence(article.content))
+	for article in range(int(len(articles) * 0.8) + 1, len(articles)):
+		x_test.append(text.text_to_word_sequence(article.content))
+
 	x_train = sequence.pad_sequences(x_train, maxlen = max_review_length)
 	x_test = sequence.pad_sequences(x_test, maxlen = max_review_length)
 
@@ -97,30 +100,21 @@ def create_datasets(raw_data, top_words, max_review_length):
 def create_model(top_words, max_review_length, embedding_size):
 	"""Create a model with the given parameters."""
 	model = Sequential()
-	model.add(Embedding(top_words, embedding_size, input_length = max_review_length))
-	#model.add(Dropout(0.2))
-	model.add(LSTM(100))
-	#model.add(Dropout(0.2))
-	model.add(Dense(1, activation = 'sigmoid'))
+	model.add(LSTM(75)) # """, input_shape = (X.shape[1], X.shape[2])"""
+	model.add(Dense(units = 1, vocab_size = top_words, activation = 'softmax'))
 
 	return model
 
 def create_neural_network(top_words, max_review_length, embedding_size, loss, optimizer, metrics):
 	"""Create a neural network that takes a dataset of texts as input and generates texts based on the dataset."""
 	model = create_model(top_words, max_review_length, embedding_size)
-	model.compile(loss = loss, optimizer = optimizer, metrics = [metrics])
+	model.compile(loss = 'categorical_crossentropy', optimizer = 'adam', metrics = ['accuracy'])
 
 	return model
 
 def train_neural_network(model, datasets, epochs, batch_size):
 	"""Train the neural network using the provided dataset."""
-	model.fit(
-		datasets.x_train,
-		datasets.y_train,
-		validation_data = (datasets.x_test, datasets.y_test),
-		epochs = epochs,
-		batch_size = batch_size
-	)
+	model.fit(datasets.x_train, (datasets.x_test, datasets.y_test), epochs = 100, verbose = 2)
 
 def generate_text(model, datasets):
 	"""Generate new text."""
@@ -131,15 +125,14 @@ def generate_text(model, datasets):
 
 def main():
 	"""Script entry point"""
-	print('hello world')
 
 	file = get_input_file()
 
-	raw_data = load_data(file)
+	articles = load_data(file)
 
 	top_words = 5000
 	max_review_length = 500
-	datasets = create_datasets(raw_data, top_words, max_review_length)
+	datasets = create_datasets(articles, top_words, max_review_length)
 
 	np.random.seed(1337)
 	model = create_neural_network(
@@ -151,7 +144,7 @@ def main():
 		'accuracy'
 	)
 
-	#train_neural_network(model, datasets, 3, 64)
+	train_neural_network(model, datasets, 3, 64)
 
 	#generate_text(model, datasets)
 
